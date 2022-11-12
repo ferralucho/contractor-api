9465eacbabef023da392f6a2b2978e1eb56c7525
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-12 17:20:15 -0300

    api base url

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -2,6 +2,7 @@ class Config {
     constructor () {
       this.env = 'development'
       this.PORT = process.env.PORT || 3000
+      this.API_BASE = '/api'
       this.CLIENT_URL = 'http://localhost:8081'
     }
   }
