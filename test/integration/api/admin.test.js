740e1e91c4c66895356ccf6585003d4b14d84aa9
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-13 12:43:46 -0300

    integration tests for admin, contracts, jobs

--- test/integration/api/admin.test.js
+++ test/integration/api/admin.test.js
@@ -3,18 +3,19 @@ const config = require('../../../src/config')
 const app = require('../../../src/app')
 const sequelize = require('../../../src/config/sequelize')
 
-const API_JOBS = `${config.API_BASE}/jobs`
-let PROFILE_ID = '8'
+const API_ADMIN = `${config.API_BASE}/admin`
+let PROFILE_ID = '8';
 
 afterAll(async () => {
   await sequelize.close()
-});
+})
 
-describe('Test the jobs path', () => {
-  test('It should get unpaid jobs', async () => {
+describe('Test the admin path', () => {
+  test('It should get unpaid admin', async () => {
     const response = await request(app)
-      .get(API_JOBS + '/unpaid')
-      .set('profile_id', PROFILE_ID)
+      .get(API_ADMIN + '/unpaid')
+      .send()
+      .set('profile_id', PROFILE_ID);
     expect(response.statusCode).toBe(200)
   })
 })
--- test/integration/api/contracts.test.js
+++ test/integration/api/contracts.test.js
@@ -1,32 +1,26 @@
-/* global beforeAll afterAll describe test expect */
 const request = require('supertest')
 const config = require('../../../src/config')
 const app = require('../../../src/app')
-const sequelize  = require('../../../src/config/sequelize')
+const sequelize = require('../../../src/config/sequelize')
 
 const API_CONTRACTS = `${config.API_BASE}/contracts`
 let PROFILE_ID = '6'
 
 afterAll(async () => {
   await sequelize.close()
-})
+});
 
 describe('Test the contracts path', () => {
   test('It should get contract by id', async () => {
-    const contract = {
-      name: 'Contract 2',
-      ch: 3020
-    }
     const response = await request(app)
-      .get(API_CONTRACTS + '/2').send(contract)
+      .get(API_CONTRACTS + '/2')
+      .send()
       .set('profile_id', PROFILE_ID)
     expect(response.statusCode).toBe(200)
-  })
+  });
 
-  test('It should get all contracts', async () => {
-    const response = await request(app)
-      .get(API_CONTRACTS)
-      .set('profile_id', PROFILE_ID)
+  test('It should get all contracts for the user', async () => {
+    const response = await request(app).get(API_CONTRACTS).set('profile_id', PROFILE_ID)
     const contracts = response.body
     expect(response.statusCode).toBe(200)
     expect(contracts.length).toBe(3)
